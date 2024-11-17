535919ff289f6995f93f9c34eafa1c24d6379fbe
SohelShahid117 <sohelshahid09@gmail.com>
2024-11-17 23:01:01 +0600

    10 min done

--- src/typeof.ts
+++ src/typeof.ts
@@ -23,20 +23,25 @@
     function logMessage(inpt:string | number | DetailsLog){
         if(typeof inpt === "string"){
             console.log("input type is string:",inpt);
+            // return inpt;
         }
         else if(typeof inpt === "number"){
             console.log("input type is number:",inpt);
         }
-        else if("message" in inpt && "level" in inpt){
+        else if("message" in inpt && "level" in inpt){  //here use in guard for type narrowing
             console.log(`level is ${inpt.level} & message is ${inpt.message}`)
         }
     }
 
     const test = logMessage("string type er input dilam");
     const test2 = logMessage(789);
+    const test3 = logMessage({
+        message:"bhai joldi google || chatgpt te jan",
+        level:"error"
+    });
     // console.log(test)
 
-    //5min done 
+    //10min done 
 
 
 
