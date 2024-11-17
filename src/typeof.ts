1cd9531eb4fe222442ce2de66ee436676287f128
SohelShahid117 <sohelshahid09@gmail.com>
2024-11-17 09:31:25 +0600

    part-2 5min done

--- src/typeof.ts
+++ src/typeof.ts
@@ -15,6 +15,29 @@
         director :"shahid"
     }
 
+    type DetailsLog = {
+        message : string,
+        level : "info" | "warn" | "error"
+    }
+
+    function logMessage(inpt:string | number | DetailsLog){
+        if(typeof inpt === "string"){
+            console.log("input type is string:",inpt);
+        }
+        else if(typeof inpt === "number"){
+            console.log("input type is number:",inpt);
+        }
+        else if("message" in inpt && "level" in inpt){
+            console.log(`level is ${inpt.level} & message is ${inpt.message}`)
+        }
+    }
+
+    const test = logMessage("string type er input dilam");
+    const test2 = logMessage(789);
+    // console.log(test)
+
+    //5min done 
+
 
 
 
