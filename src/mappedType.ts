b0ac4a51ec08ab95d026de24ea90731dbd29e47c
SohelShahid117 <sohelshahid09@gmail.com>
2024-11-18 08:07:59 +0600

    mission-1-conceptual-session-2-Problem-Solving-with-Typescript part-2 done

--- src/mappedType.ts
+++ src/mappedType.ts
@@ -1,6 +1,33 @@
 {
     //mapped type
 
+    type ToDo = {
+        task : string;
+        isCompleted : boolean;
+        dueDate : string
+    }
+
+    //now enjoy mapped in type by mapping
+    type OptionalToDo = {
+        //akane type k mapping kora hoyeche in keyof use kore 
+        // [T in keyof ToDo] : ToDo[T];
+        [T in keyof ToDo]? : ToDo[T];
+
+        //here T = task ,isCompleted,dueDate
+    }
+
+    const requiredToDo:ToDo = {
+        task :"assignment-1 for Next Level Developer",
+        isCompleted:false,
+        dueDate:"20-11-2024"
+        //akane sobolo property add kortei hobe nahoi lal batti dekabe
+    }
+
+    const requiredToDo2:OptionalToDo={
+        task:"ami assignment-1 valovabe korbo"
+        //akane sobolo property add koro || na koro no problm
+    }
+
 
 
 
---
+++ src/oop.ts
@@ -0,0 +1,40 @@
+{
+
+    //OOP
+    //class---------->property-------->method
+
+    //OOP er pillar 4t.VVI---->  
+
+    /*
+    1.inheritance--->ParentClass---->ChildClass
+    babar j property method,chele paiche setai holo inheritance.
+    eg:akti garir onekgolo property,method takbe.ai property,method bibinno garite(bus,cng,bike,car...) takbe setai holo inheritance
+    */
+
+    /*
+    2.abstraction--->bahaddarhat teke bus e utlam & pore GEC namlam but er modde bus er engine kivabe kaj korlo amar kisoi jana laglo na Ortat bitorer kaj t ki holo amar janar dorkar nai sei bitorer kaj t holo abstraction.
+    page print dilam & printing page nilam er modde printer kibabe kaj korlo kisoi janlam na.ei j printer bitore j kaj golo korlo setai holo abstraction.
+    kaoke call dilam se receive korlo & kothao bollam but mobile,network agolor functionality kisoi janina ,ai j internal kaj jegolo holo setai hoilo abstarction 
+    */
+
+    /*
+    3.encpsulation:kono akta jiniske alada kore fela.se internally kaj korbo but baire take dekabena.bairer jogot teke take alada kore fela.
+    say,ami valo programmer hobo tai ami 6 month er jonno griho bondi hoye gelam,mobile off kore dilam.ai j ami bairer jogot teke separated holam atai holo encapsulation.
+    */
+
+    /*
+    4.polymorphism:akta jinis bibinno rope j achoron kore takei bole bohoropi || polymorphism.ami kotao student,kotao developer,kotao electrical engr,kotao bsnsman,kotao chele,kotao jamai,sontander kase baba atai holo polymorphism.ami akjon but rop t amar bibinno jaigai bibinno hosse setai holo polymorphism.  
+    */
+    
+
+    
+    
+
+
+
+
+
+
+
+
+}
